695cae703459b2a20a1b4a6ace9ad4b28db819b1
Daniel Rodrigues <danielrodrigues.a113@gmail.com>
2017-07-22 00:26:23 -0300

    [bug] tornando funcional

--- js/nodejs/samples/class_1/app.js
+++ js/nodejs/samples/class_1/app.js
@@ -15,7 +15,7 @@ app.get('/produtos', function(req,res){ res.send("<h1>Página Teste</h1>") });
 app.get('/usuarios',function(req,res){
 
     // arquivo [lista.ejs] na pasta [views/produtos] ...
-    res.render("produtos/lista");
+    res.render("users/list");
 });
 
 // subir servidor ...
