f8dc57eadf40288c160ff29a60adb9f44b435e4d
Daniel Rodrigues <danielrodrigues.a113@gmail.com>
2017-08-22 01:11:21 -0300

    [django] primeiro model de migration

--- django/sample_env/sample_project/core/models.py
+++ django/sample_env/sample_project/core/models.py
@@ -1,6 +1,38 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+# coding: utf-8
 from django.db import models
 
-# Create your models here.
+
+"""
+--------------------------------------------------------------------------------
+Classe Usuarios ...
+--------------------------------------------------------------------------------
+"""
+class SampleUsers(models.Model):
+
+
+    """
+    campos ...
+    ----------------------------------------------------------------------------
+    """
+    rule = models.CharField(max_length=50, default='leitor', verbose_name='Nome')
+    name = models.CharField(max_length=50, verbose_name='Nome')
+    login = models.CharField(max_length=50, verbose_name='Login')
+    email = models.EmailField(verbose_name='Email')
+    about = models.TextField(max_length=500, verbose_name='Sobre')
+
+
+    """
+    ????????
+    ----------------------------------------------------------------------------
+    """
+    def __str__(self):
+        return self.name
+
+
+    """
+    ???????? ...
+    ----------------------------------------------------------------------------
+    """
+    # class Meta
+    # verbose_name='Usuário'
+    # verbose_name_plural='Usuários'
