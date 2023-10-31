498ea3e03c474dcc40b0d6917040fd20d2816b8e
Dniel <daniel.digitalarte@gmail.com>
2023-10-31 11:29:48 -0300

    Create basic.sql

---
+++ postgre/samples/basic.sql
@@ -0,0 +1,16 @@
+-- SELECT
+SELECT * FROM scheme_name.table_name WHERE sample_name ILIKE '%joao%';
+
+-- CREATE
+INSERT INTO scheme_name.table_name (id, nome, etc) VALUES(nextval('table_name_tbn_id_seq'::regclass), 'Sample', 'Etc...');
+
+-- UPDATE
+UPDATE scheme_name.table_name SET nome='Maria' WHERE id=666;
+
+-- DELETE
+DELETE FROM scheme_name.table_name WHERE id=666;
+
+-- MERGE
+MERGE INTO scheme_name.table_name AS tgt USING SOURCE_TABLE AS src
+ON (tgt.id=src.id) WHEN MATCHED THEN UPDATE SET tgt.nome=src.nome
+WHEN NOT MATCHED THEN INSERT (nome) VALUES (src.nome);
