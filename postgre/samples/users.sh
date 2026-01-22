18d02479f19b15ea806ec16ba18e1d320c848903
Dniel <daniel.digitalarte@gmail.com>
2026-01-22 20:49:25 -0300

    Update users.sh

--- postgre/samples/users.sh
+++ postgre/samples/users.sh
@@ -3,3 +3,5 @@ sudo -u user_name psql db_name # log without a password
 
 # PSQL TERMINAL
 ALTER USER user_name WITH PASSWORD 'new_password'; # change password
+CREATE USER username WITH SUPERUSER CREATEDB CREATEROLE LOGIN PASSWORD 'your_password'; # create super user
+CREATE ROLE username WITH SUPERUSER LOGIN PASSWORD 'your_password'; # create super user
